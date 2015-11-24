10ec6604fd599959dd0999ef9eb9b61783f04f7e
nengzhizhi <445954717@qq.com>
2015-11-24 10:52:21 +0800

    create

---
+++ qiniu.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	access_key: "zQKgGIPr-OBfpGn82vgqGF8iPeZO6qwO9LMtaJsk",
+	secret_key: "5FFoL8KBg-9l1AMEoaXIuIjKbqYlgn0eJ_y7LNhn",
+	domain: "http://7xoi20.com2.z0.glb.qiniucdn.com/",
+	space: "pptimg"
+}
